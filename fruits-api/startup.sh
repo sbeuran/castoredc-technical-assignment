6fd5eea04bb53b01148ed9df91847d983d263d8a
sbeuran <samuel.beuran98@gmail.com>
2025-06-08 17:23:25 +0100

    Remove duplicated code from fruits-api directory

--- fruits-api/startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Initialize the database
-python -m app.init_db
-
-# Start the application with Gunicorn
-gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 
