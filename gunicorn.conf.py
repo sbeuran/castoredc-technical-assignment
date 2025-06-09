59f86d9eb081baed2466737c82502e7f7b9b317a
sbeuran <samuel.beuran98@gmail.com>
2025-06-09 09:36:20 +0100

    fix: add gunicorn config and update startup script

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,13 @@
+import multiprocessing
+
+# Gunicorn config variables
 bind = "0.0.0.0:8000"
-workers = 4
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "uvicorn.workers.UvicornWorker"
 timeout = 600
+chdir = "/home/site/wwwroot"
+wsgi_app = "app.main:app"
 accesslog = "-"
 errorlog = "-"
+loglevel = "info"
 capture_output = True 
--- startup.sh
+++ startup.sh
@@ -7,5 +7,5 @@ cd /home/site/wwwroot
 export PYTHONPATH=/home/site/wwwroot
 export PORT=8000
 
-# Start Gunicorn with the correct app module
-exec gunicorn --chdir /home/site/wwwroot --bind=0.0.0.0:8000 --workers=4 --timeout=600 --access-logfile=- --error-logfile=- --log-level=info "app.main:app" 
+# Start Gunicorn with config file
+exec gunicorn -c /home/site/wwwroot/gunicorn.conf.py 
