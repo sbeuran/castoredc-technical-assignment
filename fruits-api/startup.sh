2cd5c5f3f66072abc2fe6052cfcd576f460a7e1a
sbeuran <samuel.beuran98@gmail.com>
2025-06-09 09:31:03 +0100

    fix: update application configuration and add missing endpoints

--- fruits-api/startup.sh
+++ fruits-api/startup.sh
@@ -1,1 +1,11 @@
- 
+#!/bin/bash
+
+# Set the working directory
+cd /home/site/wwwroot
+
+# Set environment variables
+export PYTHONPATH=/home/site/wwwroot
+export PORT=8000
+
+# Start Gunicorn with the correct app module
+exec gunicorn --chdir /home/site/wwwroot --bind=0.0.0.0:8000 --workers=4 --timeout=600 --access-logfile=- --error-logfile=- --log-level=info "app.main:app" 
